8f119e8b56a6d17b9a50e1aaaa101973898f6496
Matthew Kennedy <burnsidemk@gmail.com>
2023-03-28 20:36:45 -0500

    Simple ssr example

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -4,9 +4,8 @@ class OpenLibraryService
   include HTTParty
   base_uri 'openlibrary.org'
 
-  def search(query)
-    response = self.class.get('/search.json', { query: { q: query, fields: 'title,author_name,key' }, headers: { 'Accept-Encoding': 'application/json' } })
-    result = JSON.parse response.body
-    result['docs']
+  def search(query, page, fields = 'title,author_name,key')
+    response = self.class.get('/search.json', { query: { q: query, page:, fields: }, headers: { 'Accept-Encoding': 'application/json' } })
+    JSON.parse response.body if response.code == 200
   end
-end
+end 
